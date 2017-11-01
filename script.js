a4e7696ec337a35d2e6658f3cf1a0246dd7f5d7f
Adi Meirovitch <adime.support@gmail.com>
2017-11-01 10:52:45 +0200

    ex1 in conditions

---
+++ script.js
@@ -0,0 +1,13 @@
+//ex 1 in conditions
+var x = 5
+var y = 22
+
+if (x > y) {
+    console.log(x + "is bigger")
+
+} else if (y > x) {
+    console.log(y + "is bigger")
+
+} else { console.log("x & y are equal") }
+
+//ex2 in conditions
